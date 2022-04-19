e86cb49c4beac40785fc499d9ba4d81f483267e4
jaipack17 <jaikaransingh2007@outlook.com>
2022-04-19 20:01:12 +0530

    Setup VSX Project and GLFW

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+int main() {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Sidus", NULL, NULL);
+
+	if (window == NULL) {
+		cout << "Error starting the engine. Window not found." << endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	cout << "Started the engine successfully. Sidus is now running." << endl;
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window)) {
+
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	cout << "Terminated successfully.";
+
+	return 0;
+}
